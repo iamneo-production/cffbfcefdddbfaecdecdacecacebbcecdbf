5f257251ed27acdab2c5f6467944162c7744c25c
Iamneo <techadmin@iamneo.ai>
2023-09-15 12:28:44 +0000

    Backup Commit

--- 2.ps1
+++ 2.ps1
@@ -1,8 +1,6 @@
 
 $fileName = "MyTextFile.txt"
 $textContent = "Hello, this is some text content that we are writing to a file."
-
-
-Set-Content -Path $fileName -Value $textContent
+new-item $fileName -path "/home/coder/project/workspace"
 
 
--- folder1.ps1
+++ folder1.ps1
@@ -1,5 +1,15 @@
-# Define the folder and file names
-$folderName = new-item -itemtype directory myfolder -path "\home\coder\myfolder"
-$fileName = new-item newfile.txt -path "\home\coder\newfile.txt"
+
+$folderName = "NewFolder"
+$fileName = "NewFile.txt"
 
 
+$homeDirectory = [System.Environment]::GetFolderPath('UserProfile')
+
+
+New-Item -Path "$homeDirectory\$folderName" -ItemType Directory
+
+
+New-Item -Path "$homeDirectory\$folderName\$fileName" -ItemType File
+
+
+Write-Host "Folder '$folderName' and file '$fileName' created in $homeDirectory."
