121a6cde27148f105b1ff3b0ea622a46cdd43d21
Iamneo <techadmin@iamneo.ai>
2023-09-15 11:53:44 +0000

    Backup Commit

--- folder1.ps1
+++ folder1.ps1
@@ -1,15 +1,15 @@
-
+# Define the folder and file names
 $folderName = "NewFolder"
 $fileName = "NewFile.txt"
 
-
+# Get the path to your home directory
 $homeDirectory = [System.Environment]::GetFolderPath('UserProfile')
 
-
+# Create the folder in the home directory
 New-Item -Path "$homeDirectory\$folderName" -ItemType Directory
 
+# Create the file in the home directory (without the folder name)
+New-Item -Path "$homeDirectory\$fileName" -ItemType File
 
-New-Item -Path "$homeDirectory\$folderName\$fileName" -ItemType File
-
-
+# Confirm that the folder and file were created
 Write-Host "Folder '$folderName' and file '$fileName' created in $homeDirectory."
